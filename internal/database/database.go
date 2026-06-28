a61da8462441769f59d49ad61702ecce869f8a41
paragishere <paragmalvi1010@gmail.com>
2026-06-28 13:14:04 +0530

    docker intigration

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"os"
 
 	_ "modernc.org/sqlite"
 )
@@ -11,8 +12,12 @@ var DB *sql.DB
 func Connect() error {
 	var err error
 
-	DB, err = sql.Open("sqlite", "./database/Secrust.db")
+	// Create the folder if it doesn't exist
+	if err := os.MkdirAll("data", 0755); err != nil {
+		return err
+	}
 
+	DB, err = sql.Open("sqlite", "data/Secrust.db")
 	if err != nil {
 		return err
 	}
